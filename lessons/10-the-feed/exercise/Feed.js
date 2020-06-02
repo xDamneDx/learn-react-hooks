22877af7d07c75d15f8121f1bfac5727bda979a2
Владимир Ливинский <xDamneDx@Gmail.com>
2020-06-02 10:40:02 +0300

    Completed the chapter 'Implementing a status feed'

--- lessons/10-the-feed/exercise/Feed.js
+++ lessons/10-the-feed/exercise/Feed.js
@@ -1,20 +1,51 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useReducer } from "react"
 import FeedPost from "app/FeedPost"
 import { loadFeedPosts, subscribeToNewFeedPosts } from "app/utils"
 // import FeedFinal from './Feed.final'
 // export default FeedFinal
 export default Feed
 
+let feedState = null;
+
 function Feed() {
-  const [posts, setPosts] = useState([]);
-  const [date, setDate] = useState(Date.now());
-  const [quantity, setQuantity] = useState(3); 
-  const [newPosts, setNewPosts] = useState([]);
+  const [ state, dispatch ] = useReducer((state, action) => {
+    switch(action.type) {
+      case 'NEW_POSTS_LOADED': return {
+        ...state,
+        newPosts: action.newPosts
+      }
+      case 'LOAD_POSTS': return {
+        ...state,
+        posts: action.posts
+      }
+      case 'LOAD_NEW_POSTS': return {
+        ...state,
+        date: Date.now(),
+        quantity: state.quantity + state.newPosts.length
+      }
+      case 'VIEW_MORE': return {
+        ...state,
+        quantity: state.quantity + 3
+      }
+      default: throw new Error(`Unknown action: ${action.type}`)
+    }
+  }, feedState || {
+    posts: [],
+    date: Date.now(),
+    quantity: 3, 
+    newPosts: []
+  })
+
+  useEffect(() => {
+    feedState = state;
+  })
+
+  const { posts, date, quantity, newPosts } = state;
 
   useEffect(() => {
     let isCurrent = true;
     if (isCurrent) {
-      loadFeedPosts(date, quantity).then(result => setPosts(result));
+      loadFeedPosts(date, quantity).then(posts => dispatch({ type: 'LOAD_POSTS', posts }));
     };
     return () => {
       isCurrent = false;
@@ -22,11 +53,9 @@ function Feed() {
   }, [date, quantity]);
 
   useEffect(() => {
-    console.log(posts);
-  }, [posts]);
-
-  useEffect(() => {
-    return subscribeToNewFeedPosts(date, setNewPosts)
+    return subscribeToNewFeedPosts(date, (newPosts) => {
+      dispatch({ type: 'NEW_POSTS_LOADED', newPosts })
+    })
   }, [date])
 
   return (
@@ -34,8 +63,7 @@ function Feed() {
       {newPosts.length > 0 && (
         <div className="Feed_button_wrapper">
         <button onClick={() => {
-          setDate(Date.now())
-          setQuantity(quantity + newPosts.length)
+          dispatch({ type: 'LOAD_NEW_POSTS' })
         }} className="Feed_new_posts_button icon_button">
             View {newPosts.length} New Posts
           </button>
@@ -45,7 +73,9 @@ function Feed() {
       {posts.map(post => <FeedPost post={post} key={post.createdAt} />)}
 
       <div className="Feed_button_wrapper">
-        <button onClick={() => setQuantity(quantity + 3)} className="Feed_new_posts_button icon_button">View More</button>
+        <button onClick={() => {
+          dispatch({ type: 'VIEW_MORE' })
+        }} className="Feed_new_posts_button icon_button">View More</button>
       </div>
     </div>
   )
